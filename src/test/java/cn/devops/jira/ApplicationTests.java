bb1ab2f4bbb7eebc5812a6b10daa31c832e8615f
caojin <caojin223@163.com>
2022-12-15 11:07:25 +0800

    项目初始化

---
+++ src/test/java/cn/devops/jira/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.devops.jira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
